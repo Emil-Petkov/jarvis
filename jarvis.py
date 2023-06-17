5244744403a348dda17caeacb37657405d6582f6
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2023-06-17 19:06:55 +0300

    Update jarvis.py

--- jarvis.py
+++ jarvis.py
@@ -1,4 +1,6 @@
+import smtplib
 import sys
+from email.mime.text import MIMEText
 import pyttsx3
 import speech_recognition as sr
 import datetime
@@ -7,14 +9,20 @@ import cv2
 import random
 import webbrowser
 import pywhatkit as kit
-import smtplib
-import wikipedia as wikipedia
+import wikipedia
 from requests import get
+import time
 
 engine = pyttsx3.init("sapi5")
 voices = engine.getProperty("voices")
-# print(voices[0].id)
-engine.setProperty("voices", voices[0].id)
+engine.setProperty("voice", voices[1].id)
+
+# Добавете останалите контакти тук
+contacts = {
+    "number one": "E_Petkov@protonmail.com",
+    "number two": "emil.ivanchev.petkov@gmail.com",
+    "number three": "emil.ivanchev.petkov@abv.bg",
+}
 
 
 # text to speech
@@ -46,28 +54,52 @@ def takecommand():
 
 # to wish
 def wish():
-    hour = int(datetime.datetime.now().hour)
+    hour = datetime.datetime.now().hour
+    minutes = datetime.datetime.now().minute
 
-    if 0 <= hour <= 12:
-        speak("Good morning")
-    elif 12 > hour <= 18:
-        speak("Good afternoon")
+    if 0 <= hour <= 11:
+        info = f"Good morning sir. The time is currently {hour:02}:{minutes:02}. Today is {datetime.datetime.now().date()}."
+        speak(info)
+
+    elif 12 <= hour <= 18:
+        info = f"Good afternoon sir. The time is currently {hour:02}:{minutes:02}. Today is {datetime.datetime.now().date()}."
+        speak(info)
     else:
-        speak("Good evening")
+        info = f"Good evening sir. The time is currently {hour:02}:{minutes:02}. Today is {datetime.datetime.now().date()}."
+        speak(info)
 
     speak("Please tell me how can I help you sir?")
 
 
 # send email
 def send_email(to, content):
-    my_email = "emil.ivanchev.petkov@gmail.com"
-    my_password = "emil17031992"
-    server = smtplib.SMTP("smtp.gmail.com", 587)
-    server.ehlo()
-    server.starttls()
-    server.login(my_email, my_password)
-    server.sendmail(my_email, to, content)
-    server.close()
+    sender_email = 'emil.ivanchev.petkov@abv.bg'
+    receiver_email = to
+    subject = 'Test Email'
+    message = content
+
+    # Въвеждане на потребителското име и паролата на акаунта на ABV.bg
+    username = 'emil.ivanchev.petkov@abv.bg'
+    password = 'kknn44oo'
+
+    try:
+        # Създаване на съобщението
+        msg = MIMEText(message)
+        msg['Subject'] = subject
+        msg['From'] = sender_email
+        msg['To'] = receiver_email
+
+        # Настройка на SMTP сървъра на ABV.bg
+        smtp_server = 'smtp.abv.bg'
+        smtp_port = 465
+
+        # Изпращане на имейла
+        with smtplib.SMTP_SSL(smtp_server, smtp_port) as server:
+            server.login(username, password)
+            server.sendmail(sender_email, receiver_email, msg.as_string())
+        print('Email sent successfully!')
+    except Exception as e:
+        print('An error occurred while sending the email:', str(e))
 
 
 if __name__ == "__main__":
@@ -84,7 +116,7 @@ if __name__ == "__main__":
         elif "open cmd" in query:
             os.system("start cmd")
 
-        elif "open the camera" in query:
+        elif "open camera" in query:
             cap = cv2.VideoCapture(0)
             while True:
                 ret, img = cap.read()
@@ -107,11 +139,15 @@ if __name__ == "__main__":
             speak(f"Your IP address is {ip}")
 
         elif "wikipedia" in query:
-            speak("searching for information in Wikipedia...")
-            query = query.replace("wikipedia", "")
-            result = wikipedia.summary(query, sentences=2)
-            speak("according to wikipedia")
-            speak(result)
+            try:
+                speak("searching for information in Wikipedia...")
+                query = query.replace("wikipedia", "")
+                result = wikipedia.summary(query, sentences=2)
+                speak("according to wikipedia")
+                speak(result)
+            except:
+                speak("I'm sorry, I couldn't find any information in Wikipedia. Try again.")
+                pass
 
         elif "open youtube" in query:
             speak("sir, what should i search on youtube?")
@@ -132,18 +168,25 @@ if __name__ == "__main__":
         elif "open sportal.bg" in query:
             webbrowser.open("http://www.sportal.bg")
 
-        # elif "send email" in query:
-        #     try:
-        #         speak("what should i say?")
-        #         content = takecommand().lower()
-        #         to = "emil.ivanchev.petkov@gmail.com"
-        #         send_email(to, content)
-        #         speak("Email has been sent to Emil")
-        #
-        #     except Exception as e:
-        #         print(e)
+        elif "send email" in query:
+            while True:
+                speak("To whom should I send the email?")
+                contact_name = takecommand().lower()
+                try:
+                    receiver_email = contacts[contact_name]
+                    break
+                except KeyError:
+                    speak("I'm sorry, the contact is not found in the address book. Please try again.")
+
+            speak("What should be the content of the email?")
+            content = takecommand().lower()
+            send_email(receiver_email, content)
+
+        elif "open pictures" in query:
+            pictures_path = "C:\\Users\\emili\\OneDrive\\Pictures"
+            os.path.exists(pictures_path)
+            os.startfile(pictures_path)
 
         elif "turn off" in query:
             speak("thanks for using me sir, have a good day!")
             sys.exit()
-
